e6d6cef3ed817b7824ee7d7c9d4db7e9acee0f5a
TurboTeam335 <105672233+dgmascal@users.noreply.github.com>
2023-08-10 09:58:22 -0500

    visit site

--- portfolio/src/pages/Projects/Projects.js
+++ portfolio/src/pages/Projects/Projects.js
@@ -12,30 +12,30 @@ function Projects() {
       title: "appointme.",
       image: appointme,
       description: "appointme. offers a comprehensive and efficient solution for vendors by providing them with a user-friendly dashboard. This dashboard allows vendors to easily schedule clients, manage a comprehensive client database, and create detailed profiles showcasing the range of services offered along with associated costs to potential clients. With appointme., vendors can streamline their operations and enhance their ability to attract and serve clients effectively.",
-      link: "https://github.com/EFP18/AppointMe",
-      // deployedLink: "https://turboteam335.github.io/smirk/"
+      // link: "https://github.com/EFP18/AppointMe",
+      deployedLink: "https://aqueous-fortress-81692-b2b05a92fd74.herokuapp.com/"
     },
     {
       title: "SMIRK: Stock Market Insights & Ridiculous Knowledge",
       image: smirk,
       description: "SMIRK is a comprehensive stock analysis tool designed to empower investors with real-time data and insights. Leveraging an advanced stock exchange API, the application presents users with up-to-the-minute market trends, facilitating informed investment decisions. This project showcases my proficiency in using third-party APIs and creating user-friendly interfaces that handle a wide array of data.",
-      link: "https://github.com/TurboTeam335/smirk",
-      // deployedLink: "https://turboteam335.github.io/smirk/"
+      // link: "https://github.com/TurboTeam335/smirk",
+      deployedLink: "https://turboteam335.github.io/smirk/"
     },
     {
       title: "BookEnds",
       image: bookends,
       description: "BookEnds is a community-driven platform that connects book lovers and fosters a culture of sharing within neighborhoods. The platform allows users to search for available books in book-share boxes across their neighborhood. It demonstrates my skills in creating full-stack applications, focusing on user experience, and promoting community interaction.",
-      link: "https://github.com/TurboTeam335/BookEnd",
-      // deployedLink: "https://frozen-castle-12259.herokuapp.com/"
+      // link: "https://github.com/TurboTeam335/BookEnd",
+      deployedLink: "https://frozen-castle-12259.herokuapp.com/"
     },
-    {
-      title: "Portfolio",
-      image: portfolio,
-      description: "This Portfolio is a showcase of my latest accomplishments and endeavors. It epitomizes my dedication to creating polished web experiences, incorporating the latest web technologies and design principles. Feel free to explore the repository for this site to gain insights into my coding practices and design sensibilities.",
-      link: "https://github.com/TurboTeam335/dm-portfolio",
-      // deployedLink: "https://turboteam335.github.io/Weather-Dashboard/"
-    },
+    // {
+    //   title: "Portfolio",
+    //   image: portfolio,
+    //   description: "This Portfolio is a showcase of my latest accomplishments and endeavors. It epitomizes my dedication to creating polished web experiences, incorporating the latest web technologies and design principles. Feel free to explore the repository for this site to gain insights into my coding practices and design sensibilities.",
+    //   link: "https://github.com/TurboTeam335/dm-portfolio",
+    //   // deployedLink: "https://turboteam335.github.io/Weather-Dashboard/"
+    // },
 
   ]
 
@@ -55,8 +55,8 @@ function Projects() {
                   {project.description}
                 </Card.Text>
                 <div className="mt-auto d-flex justify-content-center">
-                  <Button className="btn-custom shadow-custom mx-1" variant="primary" href={project.link} target="_blank">Visit Repository</Button>
-                  {/* <Button className="btn-custom shadow-custom mx-1" variant="primary" href={project.deployedLink} target="_blank">Visit Deployed Site</Button> */}
+                  {/* <Button className="btn-custom shadow-custom mx-1" variant="primary" href={project.link} target="_blank">Visit Repository</Button> */}
+                  <Button className="btn-custom shadow-custom mx-1" variant="primary" href={project.deployedLink} target="_blank">Visit Site</Button>
                 </div>
               </Card.Body>
             </Card>
