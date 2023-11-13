76c43e8bb892d542dd4c7677e0f24ab4a4dd4636
TurboTeam335 <105672233+dgmascal@users.noreply.github.com>
2023-11-13 13:25:37 -0600

    resume update

--- portfolio/src/pages/About/About.js
+++ portfolio/src/pages/About/About.js
@@ -31,7 +31,7 @@ function About() {
 
       <a
         className="btn btn-custom shadow-custom mx-4 resume"
-        href="https://raw.githubusercontent.com/TurboTeam335/dm-portfolio/blob/main/Daniel%20Mascali%20JrDev%20Resume%20.pdf"
+        href="https://raw.githubusercontent.com/TurboTeam335/dm-portfolio/main/Daniel%20Mascali%20JrDev%20Resume%20.pdf"
         download
         target="_blank"
         rel="noopener noreferrer"
